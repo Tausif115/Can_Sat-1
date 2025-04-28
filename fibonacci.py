201d1403188f0b87dbd119cd067f9f6284242d90
Wazih Tausif <wazihtausif3@gmail.com>
2025-04-28 19:07:16 +0600

    1.1

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,8 @@
 def fib(n):
-    a, b = 0, 1
+    a, b, c = 0, 1, 1
     for _ in range(n):
         print(a, end = ' ')
-        a, b = b, a+b
+        a, b, c = b, c, a+b+c
 
 n = int(input("Enter Terms: "))
 fib(n)
